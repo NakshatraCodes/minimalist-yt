73925d038629e3d251c23218624032d4dd7ea507
NakshatraCodes <nakshatra163@gmail.com>
2021-01-27 11:58:12 +0530

    SearchBar Component Build

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search Here</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            ></input>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
